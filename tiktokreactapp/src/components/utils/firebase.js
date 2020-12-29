1ef532328bc4327af8c13fcf7ba93efaa5c99bec
Subhamoy Karmakar <subhamoykarmakar224@gmail.com>
2020-12-28 22:11:34 -0800

    Base App

---
+++ tiktokreactapp/src/components/utils/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDUHc3tw7IlYkoQrpVn9vRvG94YWLyuP88",
+  authDomain: "tiktokapp-78563.firebaseapp.com",
+  projectId: "tiktokapp-78563",
+  storageBucket: "tiktokapp-78563.appspot.com",
+  messagingSenderId: "447798879566",
+  appId: "1:447798879566:web:01dc519515edb19c1eaa1f",
+  measurementId: "G-YNWRD52GG1"
+};
